1c5171df7ffa4b09fa8a1c6d6ed65621cdc9355b
Hill FENG <yungangf@student.unimelb.edu.au>
2016-09-28 12:58:48 +1000

    1. change all socket connection to SSL connection 2. please program the client follow the tutorial     http://stilius.net/java/java_ssl.php
    3. generate a key store "mySrvKeystore" and password is 123456
    4. if u want to test the client connection, plz set the environment variable
        javax.net.ssl.keyStore = mySrvKeystore
        javax.net.ssl.keyStorePassword = 123456

--- src/server/threads/ClientMessage.java
+++ src/server/threads/ClientMessage.java
@@ -8,42 +8,42 @@ import server.util.Message;
 
 public class ClientMessage implements Runnable {
 
-	private BufferedReader reader; 
-	private BlockingQueue<Message> messageQueue;
-	
-	public ClientMessage(BufferedReader reader, BlockingQueue<Message> messageQueue) {
-		this.reader = reader;
-		this.messageQueue = messageQueue;
-	}
-	
-	@Override
-	//This thread reads messages from the client's socket input stream
-	public void run() {
-		
-			
-			System.out.println(Thread.currentThread().getName() 
-					+ " - Reading messages from client connection");
-			
-			String clientMsg = null;
-			try {
-			while ((clientMsg = reader.readLine()) != null) {
-			
-			
-				System.out.println(Thread.currentThread().getName() 
-						+ " - Message from client received: " + clientMsg);
-				Message msg = new Message(true, clientMsg);
-				messageQueue.add(msg);
-			}
-			
-			Message msg = new Message(false, "quit");
-			messageQueue.add(msg);
+    private BufferedReader reader;
+    private BlockingQueue<Message> messageQueue;
 
-		} catch (SocketException e) {
-			Message msg = new Message(false, "exit");
-			messageQueue.add(msg);
+    public ClientMessage(BufferedReader reader, BlockingQueue<Message> messageQueue) {
+        this.reader = reader;
+        this.messageQueue = messageQueue;
+    }
+
+    @Override
+    //This thread reads messages from the client's socket input stream
+    public void run() {
+
+
+        System.out.println(Thread.currentThread().getName()
+                + " - Reading messages from client connection");
+
+        String clientMsg = null;
+        try {
+            while ((clientMsg = reader.readLine()) != null) {
+
+
+                System.out.println(Thread.currentThread().getName()
+                        + " - Message from client received: " + clientMsg);
+                Message msg = new Message(true, clientMsg);
+                messageQueue.add(msg);
+            }
+
+            Message msg = new Message(false, "quit");
+            messageQueue.add(msg);
+
+        } catch (SocketException e) {
+            Message msg = new Message(false, "exit");
+            messageQueue.add(msg);
 //			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/server/threads/ServerConnection.java
+++ src/server/threads/ServerConnection.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,130 +13,132 @@ import org.json.simple.parser.ParseException;
 import server.Server;
 import server.util.Json;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
 public class ServerConnection implements Runnable {
-	
-	public ServerConnection(){
-		
-	}
-	public  void run() {
-		
-		ServerSocket listeningSocket = null;
-		try {
-			listeningSocket = new ServerSocket(Server.getInstance().getServerInfo().getManagementPort());
-		
-			while (true) {
-				
-				Socket clientSocket = listeningSocket.accept();
-				System.out.println("ask from other server");
-				BufferedReader in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream(), "UTF-8"));
-				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-					clientSocket.getOutputStream(), "UTF-8"));
-				String clientMsg = in.readLine();
-				System.out.println(clientMsg);
-				
-				fromOtherServers(out,clientMsg);
-				System.out.println("close");
-				clientSocket.close();
-				
-			}
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public ServerConnection() {
 
-	}
-	
-	
-	
-	public  void fromOtherServers(BufferedWriter writter,String input){
+    }
 
-		JSONParser parser = new JSONParser();
-		JSONObject clientJson;
-		try {
-			clientJson = (JSONObject) parser.parse(input);
+    public void run() {
 
-			String type = (String) clientJson.get("type");
+        SSLServerSocket listeningSocket = null;
+        try {
+            SSLServerSocketFactory sslServerSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+            listeningSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(Server.getInstance().getServerInfo().getManagementPort());
 
-			switch (type) {
-				case "lockindentity":
-					
-					String identity = (String) clientJson.get("identity");
-					String serverid = (String) clientJson.get("serverid");
-					String result;
-					if( Server.getInstance().getConnectedClients().contains(identity)){
 
-						result= Json.lockindentityServerFalse(identity);
-					}
-					else if(  Server.getInstance().getLockedIndetity().contains(identity)){
-						result= Json.lockindentityServerFalse(identity);
-					}
-					else{
-						Server.getInstance().addLockedIndetity(identity,serverid);
-						result= Json.lockindentityServerTrue(identity);
-					}
-				try {
-					writter.write(result+"\n");
-					writter.flush();
-					} catch (IOException e) {
-					
-					e.printStackTrace();
-					}
-					break;
-				case "releaseidentity":
+            while (true) {
 
-					String identity2 = (String) clientJson.get("identity");
-					if(clientJson.get("serverid").equals(Server.getInstance().getLockedIndetityServer(identity2))){
-						Server.getInstance().removeLockedIndetity(identity2);
-					}
-					break;
-				case "lockroomid":
-						
+                SSLSocket clientSocket = (SSLSocket) listeningSocket.accept();
+                System.out.println("ask from other server");
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        clientSocket.getInputStream(), "UTF-8"));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                        clientSocket.getOutputStream(), "UTF-8"));
+                String clientMsg = in.readLine();
+                System.out.println(clientMsg);
 
-					String roomID = (String) clientJson.get("roomid");
-					String result2;
-					if( Server.getInstance().getLockedChatrooms().contains(roomID)){
+                fromOtherServers(out, clientMsg);
+                System.out.println("close");
+                clientSocket.close();
 
-						result2= Json.lockRoomIDReplay(roomID, "false");
-					}
-					else if(  Server.getInstance().getLocalChatrooms().contains(roomID)){
-						result2= Json.lockRoomIDReplay(roomID, "false");
-					}
-					else{
-						Server.getInstance().addLockedChatrooms(roomID);
-						
-						result2= Json.lockRoomIDReplay(roomID, "true");
-					}
-					try {
-						writter.write(result2+"\n");
-						writter.flush();
-						} catch (IOException e) {
-						
-						e.printStackTrace();
-						}
-						break;
-				case "releaseroomid":
-					
-					String roomId = (String) clientJson.get("roomid");
-					String serverId2 = (String) clientJson.get("serverid");
-					String approved = (String) clientJson.get("approved");
-					if( approved.equals("true")){
-						Server.getInstance().addRemoteChatrooms(roomId,serverId2);
-					}
-					Server.getInstance().removeLockedChatrooms(roomId);
-					break;
-				case "deleteroom":
-					String roomId3 = (String) clientJson.get("roomid");
-					String serverid3 = (String) clientJson.get("serverid");
-					Server.getInstance().removeRemoteChatrooms(roomId3, serverid3);
-					break;
-			}
+            }
 
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void fromOtherServers(BufferedWriter writter, String input) {
+
+        JSONParser parser = new JSONParser();
+        JSONObject clientJson;
+        try {
+            clientJson = (JSONObject) parser.parse(input);
+
+            String type = (String) clientJson.get("type");
+
+            switch (type) {
+                case "lockindentity":
+
+                    String identity = (String) clientJson.get("identity");
+                    String serverid = (String) clientJson.get("serverid");
+                    String result;
+                    if (Server.getInstance().getConnectedClients().contains(identity)) {
+
+                        result = Json.lockindentityServerFalse(identity);
+                    } else if (Server.getInstance().getLockedIndetity().contains(identity)) {
+                        result = Json.lockindentityServerFalse(identity);
+                    } else {
+                        Server.getInstance().addLockedIndetity(identity, serverid);
+                        result = Json.lockindentityServerTrue(identity);
+                    }
+                    try {
+                        writter.write(result + "\n");
+                        writter.flush();
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+                    break;
+                case "releaseidentity":
+
+                    String identity2 = (String) clientJson.get("identity");
+                    if (clientJson.get("serverid").equals(Server.getInstance().getLockedIndetityServer(identity2))) {
+                        Server.getInstance().removeLockedIndetity(identity2);
+                    }
+                    break;
+                case "lockroomid":
+
+
+                    String roomID = (String) clientJson.get("roomid");
+                    String result2;
+                    if (Server.getInstance().getLockedChatrooms().contains(roomID)) {
+
+                        result2 = Json.lockRoomIDReplay(roomID, "false");
+                    } else if (Server.getInstance().getLocalChatrooms().contains(roomID)) {
+                        result2 = Json.lockRoomIDReplay(roomID, "false");
+                    } else {
+                        Server.getInstance().addLockedChatrooms(roomID);
+
+                        result2 = Json.lockRoomIDReplay(roomID, "true");
+                    }
+                    try {
+                        writter.write(result2 + "\n");
+                        writter.flush();
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+                    break;
+                case "releaseroomid":
+
+                    String roomId = (String) clientJson.get("roomid");
+                    String serverId2 = (String) clientJson.get("serverid");
+                    String approved = (String) clientJson.get("approved");
+                    if (approved.equals("true")) {
+                        Server.getInstance().addRemoteChatrooms(roomId, serverId2);
+                    }
+                    Server.getInstance().removeLockedChatrooms(roomId);
+                    break;
+                case "deleteroom":
+                    String roomId3 = (String) clientJson.get("roomid");
+                    String serverid3 = (String) clientJson.get("serverid");
+                    Server.getInstance().removeRemoteChatrooms(roomId3, serverid3);
+                    break;
+            }
+
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
 }
